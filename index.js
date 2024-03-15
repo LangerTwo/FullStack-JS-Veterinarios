b514d578f66ab94e9db72aa5ebd50b6ea235f1a8
LangerTwo <adrangel95@gmail.com>
2024-03-15 15:53:09 -0500

    se completo y el CRUD y de momento el backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import conectarDB from './config/db.js';
 import veterinarioRoutes from './routes/veterinarioRoutes.js'
+import pacientesRoutes from './routes/pacientesRoutes.js'
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ dotenv.config();
 conectarDB();
 
 app.use("/api/veterinarios", veterinarioRoutes);
+app.use("/api/pacientes", pacientesRoutes);
 
 const PORT = process.env.PORT || 3300
 
