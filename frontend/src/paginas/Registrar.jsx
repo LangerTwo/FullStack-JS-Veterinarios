390e8ffcd9a64b59b4d3ed6bb11c60575d2dbf72
LangerTwo <adrangel95@gmail.com>
2024-03-16 17:00:32 -0500

    se añadieron los estilos a Registrar y sus validaciones

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,6 +1,37 @@
+import { useState } from 'react'
 import { Link } from "react-router-dom"
+import Alerta from '../components/Alerta'
 
 const Registrar = () => {
+
+    const [ nombre, setNombre ] = useState('')
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [ repetirPassword, setRepetirPassword ] = useState('')
+
+    const [ alerta, SetAlerta] = useState({})
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      if ([nombre, email, password, repetirPassword].includes('')) {
+        SetAlerta({ msg: 'Hay campos vacios', error: true})
+        return;
+      }
+      if (password !== repetirPassword) {
+        SetAlerta({ msg: 'Los password son diferentes', error: true})
+        return;
+      }
+      if (password.length < 6) {
+        SetAlerta({ msg: 'El password es muy corto, agrega minimo 6 caracteres', error: true})
+        return;
+      }
+
+      SetAlerta({})
+      // Crear al usuario en la Api
+    }
+
+    const { msg } = alerta
+
     return (
       <>
         <div>
@@ -8,34 +39,35 @@ const Registrar = () => {
         </div>
 
         <div className="mt-20 md:mt-5 shadow-lg px-5 py-5 rounded-xl bg-white">
-          <form action="">
+          { msg && <Alerta alerta={alerta}/>}
+          <form action="" onSubmit={handleSubmit}>
 
               <div className="my-3">
                   <label className="uppercase text-gray-600 block text-xl font-bold">
                     Nombre
                   </label>
-                  <input type="text" placeholder="Tu Nombre" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"/>
+                  <input type="text" placeholder="Tu Nombre" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={nombre} onChange={ e => setNombre(e.target.value)}/>
               </div>
 
               <div className="my-3">
                 <label className="uppercase text-gray-600 block text-xl font-bold">
                   Email
                 </label>
-                <input type="email" placeholder="Email de Registro" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"/>
+                <input type="email" placeholder="Email de Registro" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={email} onChange={ e => setEmail(e.target.value)}/>
               </div>
 
               <div className="my-3">
                 <label className="uppercase text-gray-600 block text-xl font-bold">
                   Password
                 </label>
-                <input type="password" placeholder="Tu Password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"/>
+                <input type="password" placeholder="Tu Password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={password} onChange={ e => setPassword(e.target.value)}/>
               </div>
 
               <div className="my-3">
                 <label className="uppercase text-gray-600 block text-xl font-bold">
                   Repitir Password
                 </label>
-                <input type="password" placeholder="Repite tu Password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"/>
+                <input type="password" placeholder="Repite tu Password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={repetirPassword} onChange={ e => setRepetirPassword(e.target.value)}/>
               </div>
 
               <input type="submit" value="Crear Cuenta" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer md:w-auto" />
