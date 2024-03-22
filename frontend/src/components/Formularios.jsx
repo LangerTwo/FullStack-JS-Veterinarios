8abf1af587aa07ff4e7c75e9fc8b525ea83b0f9a
LangerTwo <adrangel95@gmail.com>
2024-03-22 17:10:22 -0500

    muetra todos los pacientes

--- frontend/src/components/Formularios.jsx
+++ frontend/src/components/Formularios.jsx
@@ -32,7 +32,8 @@ const Formularios = () => {
     const { msg } = alerta
   return (
     <>
-        <p className="text-lg text-center mb-10">
+        <h2 className="font-black text-3xl text-center">Administrador de Pacientes</h2>
+        <p className="text-xl mt-5 mb-10 text-center">
             Añade tus Pacientes y<span className="text-indigo-600 font-bold"> Administralos</span>
         </p>
 
--- frontend/src/components/Paciente.jsx
+++ frontend/src/components/Paciente.jsx
@@ -3,11 +3,34 @@
 const Paciente = ({paciente}) => {
 
     const { email, fecha, nombre, propietario, sintomas, _id } = paciente
+
+    console.log(fecha)
+
+    const formatearFecha = (fecha) => {
+        const nuevaFecha = new Date(fecha)
+        return new Intl.DateTimeFormat('es-ES', {dateStyle: 'long'}).format(nuevaFecha)
+    }
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
-      <p className="font-bold uppercase">Nombre:
-            <span className="font-normal normal-case"> {nombre}</span>
-      </p>
+        <p className="font-bold uppercase text-indigo-800 my-2">Nombre:
+            <span className="font-normal normal-case text-black"> {nombre}</span>
+        </p>
+
+        <p className="font-bold uppercase text-indigo-800 my-2">Propietario:
+            <span className="font-normal normal-case text-black"> {propietario}</span>
+        </p>
+
+        <p className="font-bold uppercase text-indigo-800 my-2">Email Contacto:
+            <span className="font-normal normal-case text-black"> {email}</span>
+        </p>
+
+        <p className="font-bold uppercase text-indigo-800 my-2">Fecha de Alta:
+            <span className="font-normal normal-case text-black"> {fecha}</span>
+        </p>
+        
+        <p className="font-bold uppercase text-indigo-800 my-2">Sintomas:
+            <span className="font-normal normal-case text-black"> {sintomas}</span>
+        </p>
     </div>
   )
 }
